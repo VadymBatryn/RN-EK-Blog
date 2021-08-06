773998fc42c3df6d1ee6c33daa0140b1a48d4e0a
VadymBatryn <vadimbatrin4@gmail.com>
2021-08-06 17:39:34 +0300

    feat: add PostScrollList component styling

---
+++ src/components/PostScrollList/PostScrollList.style.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  list: {
+    width: '100%',
+    height: '95%',
+  },
+  title: {
+    fontSize: 45,
+    fontWeight: 'bold',
+    marginHorizontal: 20,
+    marginVertical: 10,
+    color: '#2874A0',
+  },
+});

810c9b17769b4b347591f41e3a4e845923d51b01
VadymBatryn <vadimbatrin4@gmail.com>
2021-08-06 17:37:31 +0300

    feat: add CategoriesList component styling

---
+++ src/components/CategoriesList/CategoriesList.style.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import {SCREEN_HEIGHT} from '../../constants/constants';
+
+export default StyleSheet.create({
+  list: {
+    flexDirection: 'row',
+    height: SCREEN_HEIGHT / 20,
+    marginVertical: 10,
+  },
+});
